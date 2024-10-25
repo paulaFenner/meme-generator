910a48eadee65453eef8ea6dca3d4dd5322b5698
Paula Fenner <fenner.paula@gmail.com>
2024-10-25 16:09:24 +0200

    First commit.

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img src="../public/assets/troll-face.png" className="header-image" />
+      <h1 className="header-title">MemeGenerator</h1>
+      <h2 className="header-project">React Course - Project 3</h2>
+    </header>
+  );
+}
